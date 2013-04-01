ae3fa58dfc9b7e2936f5e9615c5780faae94e254
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2013-04-01 09:42:42 -0500

    Add AER entry points

--- block/sop.c
+++ block/sop.c
@@ -97,6 +97,21 @@ static const struct block_device_operations sop_fops = {
 #endif
 };
 
+static pci_ers_result_t sop_pci_error_detected(struct pci_dev *dev,
+				enum pci_channel_state error);
+static pci_ers_result_t sop_pci_mmio_enabled(struct pci_dev *dev);
+static pci_ers_result_t sop_pci_link_reset(struct pci_dev *dev);
+static pci_ers_result_t sop_pci_slot_reset(struct pci_dev *dev);
+static void sop_pci_resume(struct pci_dev *dev);
+
+static struct pci_error_handlers sop_pci_error_handlers = {
+	.error_detected = sop_pci_error_detected,
+	.mmio_enabled = sop_pci_mmio_enabled,
+	.link_reset = sop_pci_link_reset,
+	.slot_reset = sop_pci_slot_reset,
+	.resume = sop_pci_resume,
+};
+
 #define	SOP_DBG_LVL_EXTRA_WARN		0x0001
 #define	SOP_DBG_LVL_RARE_NORM_EVENT	0x0002
 #define SOP_DBG_LVL_DUMP_SGIO		0x0004
@@ -2156,6 +2171,7 @@ static struct pci_driver sop_pci_driver = {
 	.shutdown = sop_shutdown,
 	.suspend = sop_suspend,
 	.resume = sop_resume,
+	.err_handler = &sop_pci_error_handlers,
 };
 
 static int __init sop_init(void)
@@ -3969,6 +3985,46 @@ static int sop_getgeo(struct block_device *bdev, struct hd_geometry *geo)
 	if ((sector_t) (geo->cylinders + 1) * geo->heads * geo->sectors < nsect)
 		geo->cylinders = 0xffff;
 	return 0;
+}
+
+static pci_ers_result_t sop_pci_error_detected(struct pci_dev *dev,
+				enum pci_channel_state error)
+{
+	dev_warn(&dev->dev,
+		"sop_pci_error_detected called but not implemented\n");
+	/* FIXME: implement this. */
+	return PCI_ERS_RESULT_NONE;
+}
+
+static pci_ers_result_t sop_pci_mmio_enabled(struct pci_dev *dev)
+{
+	dev_warn(&dev->dev,
+		"sop_pci_error_mmio_enabled called but not implemented\n");
+	/* FIXME: implement this. */
+	return PCI_ERS_RESULT_NONE;
+}
+
+static pci_ers_result_t sop_pci_link_reset(struct pci_dev *dev)
+{
+	dev_warn(&dev->dev,
+		"sop_pci_error_link_reset called but not implemented\n");
+	/* FIXME: implement this. */
+	return PCI_ERS_RESULT_NONE;
+}
+
+static pci_ers_result_t sop_pci_slot_reset(struct pci_dev *dev)
+{
+	dev_warn(&dev->dev,
+		"sop_pci_error_slot_reset called but not implemented\n");
+	/* FIXME: implement this. */
+	return PCI_ERS_RESULT_NONE;
+}
+
+static void sop_pci_resume(struct pci_dev *dev)
+{
+	dev_warn(&dev->dev, "sop_pci_resume called but not implemented\n");
+	/* FIXME: implement this. */
+	return;
 }
 
 /* This gets optimized away, but will fail to compile if we mess up
