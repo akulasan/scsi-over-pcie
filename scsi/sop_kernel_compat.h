c26988b3acbeb7867aba8b4f3fb723eed9bbaed4
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2013-04-12 12:43:00 -0500

    fix up scsi driver kernel compat file to compile with newer kernel

--- scsi/sop_kernel_compat.h
+++ scsi/sop_kernel_compat.h
@@ -26,4 +26,17 @@ void usleep_range(unsigned long min, unsigned long max)
 }
 #endif
 
+/* these next three disappeared in 3.8-rc4 */
+#ifndef __devinit
+#define __devinit
+#endif
+
+#ifndef __devexit
+#define __devexit
+#endif
+
+#ifndef __devexit_p
+#define __devexit_p(x) x
+#endif
+
 #endif
